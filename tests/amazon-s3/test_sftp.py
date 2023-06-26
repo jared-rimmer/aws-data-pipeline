f7dbbe74e929a8d8623e72a166656895dd6ab667
jared-rimmer <jared.rimmer@brooklyndata.co>
2023-06-26 15:13:16 +0100

    Add sftp and s3 functions with tests

---
+++ tests/amazon-s3/test_sftp.py
@@ -0,0 +1,36 @@
+from pipeline.config.connections import get_sftp_connection_credentials
+from pipeline.utils.sftp import sftp_connection
+from pipeline.scripts.load_data_from_sftp_to_s3 import list_files_in_sftp, get_new_files, upload_data_from_sftp_to_s3
+
+
+from unittest.mock import patch, MagicMock
+
+def test_file_list(sftp_credentials):
+    with sftp_connection(get_sftp_connection_credentials()) as sftp_conn:
+        with patch.object(sftp_conn, 'stat', new_callable=MagicMock()) as stat:       
+            stat.return_value = MagicMock(st_mtime=1687516497)   
+
+            result = list_files_in_sftp(sftp_conn, path='upload')   
+
+            assert result == [
+                {'file': '2023-06-23-trades.csv', 'modified': 1687516497},
+                {'file': '2023-06-24-trades.csv', 'modified': 1687516497},
+                {'file': '2023-06-25-trades.csv', 'modified': 1687516497},
+            ]
+
+
+def test_get_new_files():
+
+    test_files = [
+        {'file': '2023-06-23-trades.csv', 'modified': 1687516497},
+        {'file': '2023-06-24-trades.csv', 'modified': 1687783811},
+        {'file': '2023-06-25-trades.csv', 'modified': 1687783815},
+
+    ]
+
+    result = get_new_files(list_of_files=test_files, last_modified=1687516498)
+
+    assert result == [
+        {'file': '2023-06-24-trades.csv', 'modified': 1687783811},
+        {'file': '2023-06-25-trades.csv', 'modified': 1687783815}
+    ]

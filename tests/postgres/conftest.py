c827c8c0c496c46a62790eaaa97df5eff78a65cd
jared-rimmer <jared.rimmer@brooklyndata.co>
2023-07-03 16:43:47 +0100

    Revert "Refactor localhost reference to 127.0.0.1"
    This reverts commit 39812c61b0cf8d261bdda65211128839fae7e891.

--- tests/postgres/conftest.py
+++ tests/postgres/conftest.py
@@ -7,6 +7,6 @@ def postgres_credentials():
     """Mocked SFTP Credentials"""
     os.environ['DATABASE_USERNAME'] = 'postgres'
     os.environ['DATABASE_PASSWORD'] = 'postgres'
-    os.environ['DATABASE_HOST'] = '127.0.0.1'
+    os.environ['DATABASE_HOST'] = 'localhost'
     os.environ['DATABASE_NAME'] = 'postgres'
     os.environ['DATABASE_PORT'] = '5432'

2d09d32058074afbda107d68dc2d20e650b44e5b
jared-rimmer <jared.rimmer@brooklyndata.co>
2023-07-12 14:56:05 +0100

    Remove psycopg2-binary and add pg8000

--- tests/postgres/test_postgres.py
+++ tests/postgres/test_postgres.py
@@ -9,8 +9,8 @@ import pytest
 def setup():
     with database_connection(get_postgres_connection_credentials()) as connection:
 
-        connection.execute(f"""truncate table production.trades;""")
-        connection.execute("""
+        connection.run(f"""truncate table production.trades;""")
+        connection.run("""
             INSERT INTO production.trades (id, ticker, price, quantity, status, source_file_name)
             VALUES 
                 (1, 'TSLA', 100.00, 10, 'OPEN', '2023-06-23-trades-1'),
@@ -36,7 +36,8 @@ def test_postgres_client_delete_sources(setup):
         client = PostgresClient(connection=connection)
         client.delete_sources(schema='production', table_name='trades', sources=['2023-06-24-trades-1'])
 
-        connection.execute(f"""select distinct source_file_name from production.trades;""")
-        result = sorted(set([source[0] for source in connection.fetchall()]))
-
-        assert result == ['2023-06-23-trades-1', '2023-06-23-trades-2']
+        test = sorted(set(row[0] for row in connection.run(f"""select distinct source_file_name from production.trades;""")))
+        
+        # result = sorted(set([source[0] for source in connection.fetchall()]))
+      
+        assert test == ['2023-06-23-trades-1', '2023-06-23-trades-2']
